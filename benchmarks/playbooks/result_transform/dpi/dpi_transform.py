facfc482e79dd46580d661e4ddc20c8cec77a13e
Nauman_Ahad <Nauman_Ahad@dell.com>
2015-11-05 19:05:35 +0500

    Ansible compute benchmark playbooks  with system Information and Result Transformation
    Inclused update playbooks for SSL,DPI, Dhrystone,whetstone
    Also included python scripts for system information collection and Result Transformation.
    JIRA:QTIP-20
    JIRA:QTIP-36
    JIRA:QTIP-37
    JIRA:QTIP-39
    JIRA:QTIP-40
    JIRA:QTIP-52
    
    Change-Id: Ic618dfd8573f49ff9db7a00dfc2f7ac015713dec
    Signed-off-by: Nauman_Ahad <Nauman_Ahad@dell.com>

---
+++ benchmarks/playbooks/result_transform/dpi/dpi_transform.py
@@ -0,0 +1,50 @@
+import os
+import json
+import pickle
+import datetime
+
+sum_dpi_pps = float(0)
+sum_dpi_bps = float(0)
+
+for x in range(1, 11):
+    dpi_result_pps = float(
+        os.popen(
+            "cat $HOME/qtip_result/dpi_dump.txt | grep 'nDPI throughput:' | awk 'NR=='" +
+            str(x) +
+            " | awk '{print $3}'").read().lstrip())
+    dpi_result_bps = float(
+        os.popen(
+            "cat $HOME/qtip_result/dpi_dump.txt | grep 'nDPI throughput:' | awk 'NR=='" +
+            str(x) +
+            " | awk '{print $7}'").read().rstrip())
+
+    if (dpi_result_pps > 100):
+        dpi_result_pps = dpi_result_pps / 1000
+
+    if (dpi_result_bps > 100):
+        dpi_result_bps = dpi_result_bps / 1000
+
+    sum_dpi_pps += dpi_result_pps
+    sum_dpi_bps += dpi_result_bps
+
+dpi_result_pps = sum_dpi_pps / 10
+dpi_result_bps = sum_dpi_bps / 10
+
+host = os.popen("hostname").read().rstrip()
+log_time_stamp = str(datetime.datetime.utcnow().isoformat())
+
+os.popen(
+    "cat $HOME/qtip_result/dpi_dump.txt > $HOME/qtip_result/" +
+    host +
+    "-" +
+    log_time_stamp +
+    ".log")
+
+home_dir = str(os.popen("echo $HOME").read().rstrip())
+host = os.popen("echo $HOSTNAME")
+result = {}
+result['DPI_benchmark(M pps)'] = round(dpi_result_pps, 3)
+result['DPI_benchmark(Gb/s)'] = round(dpi_result_bps, 3)
+with open('./result_temp', 'w+') as result_file:
+    pickle.dump(result, result_file)
+

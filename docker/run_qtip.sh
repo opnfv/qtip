11c81d90581606c0bde98e04a4f739eed4bcbf6d
Nauman_Ahad <Nauman_Ahad@dell.com>
2016-01-07 13:09:45 +0500

    Slight changes to the run_qtip.sh script
    Modifications needed in run_qtip.sh for Jenkins Jobs
    to run. Docker file changed too.
    get_env.sh script location changed too.
    
    Testcases for dell-us-deploying-bm-3 lab added along with the default testcase
    directory
    
    Change-Id: I293febda0ddd3d56d1c455fde220dc78a57b6c18
    Signed-off-by: Nauman_Ahad <Nauman_Ahad@dell.com>

--- docker/run_qtip.sh
+++ docker/run_qtip.sh
@@ -1,16 +1,39 @@
 #! /bin/bash
 
+
+
 cp ${REPOS_DIR}/releng/utils/fetch_os_creds.sh ${QTIP_DIR}/data/
-cd ${QTIP_DIR}/data  &&  source get_env_info.sh -n ${INSTALLER_TYPE} -i ${INSTALLER_IP}
+cd ${QTIP_DIR}  &&  source get_env_info.sh \
+-n ${INSTALLER_TYPE} \
+-i ${INSTALLER_IP}
+
 source ${QTIP_DIR}/data/opnfv-creds.sh
-cd ${QTIP_DIR}  && python qtip.py -l ${LAB} -f compute.txt
-cd ${QTIP_DIR}  && python qtip.py -l ${LAB} -f storage.txt
-cd ${QTIP_DIR}  && python qtip.py -l ${LAB} -f network.txt
 
-cd ${QTIP_DIR}/data/ref_results/ $$ python compute_suite.py
-cd ${QTIP_DIR}/data/ref_results/ $$ python storage_suite.py
-cd ${QTIP_DIR}/data/ref_results/ $$ python network_suite.py
+if [ "$TEST_CASE" == "compute" ]; then
+    cd ${QTIP_DIR}  && python qtip.py -l ${NODE_NAME} -f compute.txt
+    cd ${QTIP_DIR}/data/ref_results/ $$ python compute_suite.py
+fi
 
+if [ "$TEST_CASE" == "storage" ]; then
+    cd ${QTIP_DIR}  && python qtip.py -l ${NODE_NAME} -f storage.txt
+    cd ${QTIP_DIR}/data/ref_results/ $$ python storage_suite.py
+fi
+
+if [ "$TEST_CASE" == "network" ]; then
+    cd ${QTIP_DIR}  && python qtip.py -l ${NODE_NAME} -f network.txt
+    cd ${QTIP_DIR}/data/ref_results/ $$ python network_suite.py
+fi
+
+
+if [ "$TEST_CASE" == "all" ]; then
+    cd ${QTIP_DIR}  && python qtip.py -l ${NODE_NAME} -f compute.txt
+    cd ${QTIP_DIR}  && python qtip.py -l ${NODE_NAME} -f storage.txt
+    cd ${QTIP_DIR}  && python qtip.py -l ${NODE_NAME} -f network.txt
+
+    cd ${QTIP_DIR}/data/ref_results/ $$ python compute_suite.py
+    cd ${QTIP_DIR}/data/ref_results/ $$ python storage_suite.py
+    cd ${QTIP_DIR}/data/ref_results/ $$ python network_suite.py
+fi
 
 
 

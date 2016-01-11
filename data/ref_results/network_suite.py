3c194a8bf95ab68d440b68587ba7f36a921116de
Nauman_Ahad <Nauman_Ahad@dell.com>
2016-01-11 15:09:12 +0500

    Modifications to the index calculation script
    Modifications to the index calulation script.
    Key for each suite changed to "Index" from  "<suite> Index".
    This is to reuse code for the dashboard.py file for QTIP
    
    Change-Id: I6e8c8c268b7d9376b0f44d572ed022910e8ad4d9
    Signed-off-by: Nauman_Ahad <Nauman_Ahad@dell.com>

--- data/ref_results/network_suite.py
+++ data/ref_results/network_suite.py
@@ -15,7 +15,7 @@ for benchmark in network_bench_list:
 network_suite_index=temp/len(network_bench_list)
 
 network_dict_f={};
-network_dict_f['1. Network Index']=network_suite_index
+network_dict_f['1. Index']=network_suite_index
 network_dict_f['2. Network suite results']=network_dict
 with open('../../results/network_result.json', 'w+') as result_json:
     json.dump(network_dict_f, result_json, indent=4, sort_keys=True)

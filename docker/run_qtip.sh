bec658afadacfeeca9fbfbe598ceb5c5601a3372
Nauman_Ahad <Nauman_Ahad@dell.com>
2016-01-13 19:45:52 +0500

    run_qtip.sh fix
    Renamed compute.txt to compute for run_qtip.sh
    to reflect changes made in qtip repo
    
    Change-Id: I4456ef305cfd01c039ce152b5fd6f6512204166d
    Signed-off-by: Nauman_Ahad <Nauman_Ahad@dell.com>

--- docker/run_qtip.sh
+++ docker/run_qtip.sh
@@ -10,25 +10,25 @@ cd ${QTIP_DIR}  &&  source get_env_info.sh \
 source ${QTIP_DIR}/opnfv-creds.sh
 
 if [ "$TEST_CASE" == "compute" ]; then
-    cd ${QTIP_DIR}  && python qtip.py -l ${NODE_NAME} -f compute.txt
+    cd ${QTIP_DIR}  && python qtip.py -l ${NODE_NAME} -f compute
     cd ${QTIP_DIR}/data/ref_results/ $$ python compute_suite.py
 fi
 
 if [ "$TEST_CASE" == "storage" ]; then
-    cd ${QTIP_DIR}  && python qtip.py -l ${NODE_NAME} -f storage.txt
+    cd ${QTIP_DIR}  && python qtip.py -l ${NODE_NAME} -f storage
     cd ${QTIP_DIR}/data/ref_results/ $$ python storage_suite.py
 fi
 
 if [ "$TEST_CASE" == "network" ]; then
-    cd ${QTIP_DIR}  && python qtip.py -l ${NODE_NAME} -f network.txt
+    cd ${QTIP_DIR}  && python qtip.py -l ${NODE_NAME} -f network
     cd ${QTIP_DIR}/data/ref_results/ $$ python network_suite.py
 fi
 
 
 if [ "$TEST_CASE" == "all" ]; then
-    cd ${QTIP_DIR}  && python qtip.py -l ${NODE_NAME} -f compute.txt
-    cd ${QTIP_DIR}  && python qtip.py -l ${NODE_NAME} -f storage.txt
-    cd ${QTIP_DIR}  && python qtip.py -l ${NODE_NAME} -f network.txt
+    cd ${QTIP_DIR}  && python qtip.py -l ${NODE_NAME} -f compute
+    cd ${QTIP_DIR}  && python qtip.py -l ${NODE_NAME} -f storage
+    cd ${QTIP_DIR}  && python qtip.py -l ${NODE_NAME} -f network
 
     cd ${QTIP_DIR}/data/ref_results/ $$ python compute_suite.py
     cd ${QTIP_DIR}/data/ref_results/ $$ python storage_suite.py

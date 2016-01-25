0e745438997721838b763b089b560ddb0c53d98b
MofassirArif <Mofassir_Arif@dellteam.com>
2016-01-25 03:56:21 -0800

    update: address for testing db
    Change-Id: I48b30fffd1de2512e4f46ee951a85299edc06a1c
    Signed-off-by: MofassirArif <Mofassir_Arif@dellteam.com>

--- dashboard/pushtoDB.py
+++ dashboard/pushtoDB.py
@@ -2,7 +2,7 @@ import requests
 import json
 import datetime
 import os
-TEST_DB = 'http://213.77.62.197'
+TEST_DB = 'http://testresults.opnfv.org/testapi'
 
 suite_list = [('compute_result.json', 'compute_test_suite'),('network_result.json', 'network_test_suite'),('storage_result.json', 'storage_test_suite')]
 payload_list = { }
--- docker/run_qtip.sh
+++ docker/run_qtip.sh
@@ -11,17 +11,17 @@ source ${QTIP_DIR}/opnfv-creds.sh
 
 if [ "$TEST_CASE" == "compute" ]; then
     cd ${QTIP_DIR}  && python qtip.py -l ${NODE_NAME} -f compute
-    cd ${QTIP_DIR}/data/ref_results/ $$ python compute_suite.py
+    cd ${QTIP_DIR}/data/ref_results/ && python compute_suite.py
 fi
 
 if [ "$TEST_CASE" == "storage" ]; then
     cd ${QTIP_DIR}  && python qtip.py -l ${NODE_NAME} -f storage
-    cd ${QTIP_DIR}/data/ref_results/ $$ python storage_suite.py
+    cd ${QTIP_DIR}/data/ref_results/ && python storage_suite.py
 fi
 
 if [ "$TEST_CASE" == "network" ]; then
     cd ${QTIP_DIR}  && python qtip.py -l ${NODE_NAME} -f network
-    cd ${QTIP_DIR}/data/ref_results/ $$ python network_suite.py
+    cd ${QTIP_DIR}/data/ref_results/ && python network_suite.py
 fi
 
 
@@ -30,9 +30,9 @@ if [ "$TEST_CASE" == "all" ]; then
     cd ${QTIP_DIR}  && python qtip.py -l ${NODE_NAME} -f storage
     cd ${QTIP_DIR}  && python qtip.py -l ${NODE_NAME} -f network
 
-    cd ${QTIP_DIR}/data/ref_results/ $$ python compute_suite.py
-    cd ${QTIP_DIR}/data/ref_results/ $$ python storage_suite.py
-    cd ${QTIP_DIR}/data/ref_results/ $$ python network_suite.py
+    cd ${QTIP_DIR}/data/ref_results/ && python compute_suite.py
+    cd ${QTIP_DIR}/data/ref_results/ && python storage_suite.py
+    cd ${QTIP_DIR}/data/ref_results/ && python network_suite.py
 fi
 
 

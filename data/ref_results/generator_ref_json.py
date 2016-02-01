b882c9c54ce8e6c91c5f30bd394eec3934011549
Nauman_Ahad <nauman_ahad@xflowresearch.com>
2016-02-01 17:03:50 +0500

    Fixed the index calculation for QTIP compute, network and storage suites
    Earlier inaccurate index calculation updated.
    Now indices take into account vm and bm cases
    
    Change-Id: Icf2d7fc17ef765f5c0611b5371d57410ba155cb6
    Signed-off-by: Nauman_Ahad <nauman_ahad@xflowresearch.com>
    (cherry picked from commit 49b4662f32c415ea141ff269ba39d9a401b6e320)

--- data/ref_results/generator_ref_json.py
+++ data/ref_results/generator_ref_json.py
@@ -3,39 +3,83 @@ import json
 
 dict_ref={};
 dict_ref['compute']={};
-dict_ref['compute']['dpi']=8.12
-dict_ref['compute']['whetstone']=859.1
-dict_ref['compute']['dhrystone']=3146.66
-dict_ref['compute']['ssl']={};
-dict_ref['compute']['ssl']['RSA']={};
-dict_ref['compute']['ssl']['AES']={};
-dict_ref['compute']['ssl']['RSA']['512b']=22148.9
-dict_ref['compute']['ssl']['RSA']['1024b']=7931.44
-dict_ref['compute']['ssl']['RSA']['2048b']=1544.3
-dict_ref['compute']['ssl']['RSA']['4096b']=161.92
+dict_ref['compute']['dpi_bm']=8.12
+dict_ref['compute']['dpi_vm']=22.12
 
-dict_ref['compute']['ssl']['AES']['16B']=735490250
-dict_ref['compute']['ssl']['AES']['64B']=788429210
-dict_ref['compute']['ssl']['AES']['256B']=803323650
-dict_ref['compute']['ssl']['AES']['1024B']=808861020
-dict_ref['compute']['ssl']['AES']['8192B']=807701160
+dict_ref['compute']['whetstone_bm']={};
+dict_ref['compute']['whetstone_vm']={};
+dict_ref['compute']['whetstone_bm']['single_cpu']=859.1
+dict_ref['compute']['whetstone_bm']['multi_cpu']=859.1
+dict_ref['compute']['whetstone_vm']['single_cpu']=859.1
+dict_ref['compute']['whetstone_vm']['multi_cpu']=859.
 
-dict_ref['compute']['ramspeed']={};
-dict_ref['compute']['ramspeed']['INTmem']={};
-dict_ref['compute']['ramspeed']['FLOATmem']={};
-dict_ref['compute']['ramspeed']['INTmem']['Average (MB/s)']=11775.85
-dict_ref['compute']['ramspeed']['FLOATmem']['Average (MB/s)']=9780.23
+dict_ref['compute']['dhrystone_bm']={};
+dict_ref['compute']['dhrystone_vm']={};
+dict_ref['compute']['dhrystone_bm']['single_cpu']=3231.7
+dict_ref['compute']['dhrystone_bm']['multi_cpu']=103362.1
+dict_ref['compute']['dhrystone_vm']['single_cpu']=2953.6
+dict_ref['compute']['dhrystone_vm']['multi_cpu']=10585.8
+
+dict_ref['compute']['ssl_bm']={};
+dict_ref['compute']['ssl_bm']['RSA']={};
+dict_ref['compute']['ssl_bm']['AES']={};
+dict_ref['compute']['ssl_bm']['RSA']['512b']=22148.9
+dict_ref['compute']['ssl_bm']['RSA']['1024b']=7931.44
+dict_ref['compute']['ssl_bm']['RSA']['2048b']=1544.3
+dict_ref['compute']['ssl_bm']['RSA']['4096b']=161.92
+dict_ref['compute']['ssl_bm']['AES']['16B']=735490250
+dict_ref['compute']['ssl_bm']['AES']['64B']=788429210
+dict_ref['compute']['ssl_bm']['AES']['256B']=803323650
+dict_ref['compute']['ssl_bm']['AES']['1024B']=808861020
+dict_ref['compute']['ssl_bm']['AES']['8192B']=807701160
+
+dict_ref['compute']['ssl_vm']={};
+dict_ref['compute']['ssl_vm']['RSA']={};
+dict_ref['compute']['ssl_vm']['AES']={};
+dict_ref['compute']['ssl_vm']['RSA']['512b']=22148.9
+dict_ref['compute']['ssl_vm']['RSA']['1024b']=7931.44
+dict_ref['compute']['ssl_vm']['RSA']['2048b']=1544.3
+dict_ref['compute']['ssl_vm']['RSA']['4096b']=161.92
+dict_ref['compute']['ssl_vm']['AES']['16B']=735490250
+dict_ref['compute']['ssl_vm']['AES']['64B']=788429210
+dict_ref['compute']['ssl_vm']['AES']['256B']=803323650
+dict_ref['compute']['ssl_vm']['AES']['1024B']=808861020
+dict_ref['compute']['ssl_vm']['AES']['8192B']=807701160
+
+
+dict_ref['compute']['ramspeed_bm']={};
+dict_ref['compute']['ramspeed_bm']['INTmem']={};
+dict_ref['compute']['ramspeed_bm']['FLOATmem']={};
+dict_ref['compute']['ramspeed_bm']['INTmem']['Average (MB/s)']=11775.85
+dict_ref['compute']['ramspeed_bm']['FLOATmem']['Average (MB/s)']=9780.23
+
+dict_ref['compute']['ramspeed_vm']={};
+dict_ref['compute']['ramspeed_vm']['INTmem']={};
+dict_ref['compute']['ramspeed_vm']['FLOATmem']={};
+dict_ref['compute']['ramspeed_vm']['INTmem']['Average (MB/s)']=11775.85
+dict_ref['compute']['ramspeed_vm']['FLOATmem']['Average (MB/s)']=9780.23
+
 
 dict_ref['storage']={};
-dict_ref['storage']['read']={};
-dict_ref['storage']['write']={};
-dict_ref['storage']['read']['IOPS']= 6995
-dict_ref['storage']['write']['IOPS']= 6990
+dict_ref['storage']['fio_bm']={};
+dict_ref['storage']['fio_bm']['read']={};
+dict_ref['storage']['fio_bm']['write']={};
+dict_ref['storage']['fio_bm']['read']['IOPS']=6693
+dict_ref['storage']['fio_bm']['write']['IOPS']=6688
+
+dict_ref['storage']['fio_vm']={};
+dict_ref['storage']['fio_vm']['read']={};
+dict_ref['storage']['fio_vm']['write']={};
+dict_ref['storage']['fio_vm']['read']['IOPS']=2239
+dict_ref['storage']['fio_vm']['write']['IOPS']=2237
 
 dict_ref['network']={};
-dict_ref['network']['iperf']={};
-dict_ref['network']['iperf']['throughput received(b/s)']=9973180000.0
-
+dict_ref['network']['iperf_bm']={};
+dict_ref['network']['iperf_vm']={};
+dict_ref['network']['iperf_vm_2']={};
+dict_ref['network']['iperf_bm']['throughput received(b/s)']=944473000.0
+dict_ref['network']['iperf_vm']['throughput received(b/s)']=14416700000.0
+dict_ref['network']['iperf_vm_2']['throughput received(b/s)']=2461530000.0
 with open('reference.json', 'w+') as result_json:
     json.dump(dict_ref, result_json, indent=4, sort_keys=True)
 
--- data/ref_results/network_benchmarks_indices.py
+++ data/ref_results/network_benchmarks_indices.py
@@ -1,28 +1,27 @@
-import os
-import json
+from index_calculation import generic_index as get_index
+from index_calculation import get_reference 
 from result_accum import result_concat as concat
 
+
 def iperf_index ():
-    total_r=0
     iperf_dict=concat('../../results/iperf/')
-    for k,v in iperf_dict.iteritems():
-        for i,j in iperf_dict[k].iteritems():
-            if i=="3  IPERF result":
-               for a,b in iperf_dict[k][i].iteritems():        
-                   if a=="2. Bandwidth":
-                      raw_num=iperf_dict[k][i][a]['2. throughput Received (b/s)']
-                      total_r=total_r+raw_num
+    #print iperf_dict    
+    iperf_bm_ref = get_reference('network','iperf_bm','throughput received(b/s)')
+ 
+    iperf_bm_index= get_index(iperf_dict,'iperf_bm',iperf_bm_ref,'4  IPERF result', '2. Bandwidth','2. throughput Received (b/s)')
+   
+    iperf_vm_ref = get_reference('network','iperf_vm','throughput received(b/s)')
+    iperf_vm_index= get_index(iperf_dict,'iperf_vm',iperf_vm_ref,'4  IPERF result', '2. Bandwidth','2. throughput Received (b/s)')    
+    
+    iperf_vm_2_ref = get_reference('network','iperf_vm_2','throughput received(b/s)')
+    iperf_vm_2_index= get_index(iperf_dict,'iperf_vm_2',iperf_vm_2_ref,'4  IPERF result', '2. Bandwidth','2. throughput Received (b/s)')        
+     
 
-    l= len(iperf_dict)
-
-    with open ('./reference.json') as reference_file:
-        reference_djson=json.load(reference_file)
-        iperf_ref_r=reference_djson['network']['iperf']['throughput received(b/s)']
-       
-
-    iperf_index_r=float((total_r/l)/iperf_ref_r)
+    
+    iperf_index= float(iperf_bm_index+iperf_vm_index+iperf_vm_2_index)/3
+    print iperf_index
     iperf_dict_i={};
-    iperf_dict_i['1. Index']=iperf_index_r
+    iperf_dict_i['1. Index']=iperf_index
     iperf_dict_i['2. Results']=iperf_dict
     return iperf_dict_i
-
+    

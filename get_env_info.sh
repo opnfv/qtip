bde153c645f0ef9decbdce115f27415debd3d237
Nauman_Ahad <nauman.ahad@xflowresearch.com>
2016-02-23 15:24:29 +0500

    Resolved bug for Compass CI Jobs
    Resolved problem for ssh into baremetal nodes.
    Capability for CI Jobs through Apex also added
    
    Change-Id: Ide34b97f97269750e8ec3d6f18ca693d28359223
    Signed-off-by: Nauman_Ahad <nauman.ahad@xflowresearch.com>

--- get_env_info.sh
+++ get_env_info.sh
@@ -1,22 +1,37 @@
 #! /bin/bash
 
 usage() {
-    echo "usage $0  -n <installer_type> -i <installer_ip>"
+    echo "usage $0  -n <installer_type> -i <installer_ip> -k <key incase of apex>"
 }
 
 
 
-while getopts ":n:i:" optchar; do
+while getopts ":n:i:k:" optchar; do
     case "${optchar}" in
         n)
          export INSTALLER_TYPE=${OPTARG};; 
 
         i)
          export  INSTALLER_IP=${OPTARG};;
+     
+        k)
+         export APEX_KEY=${OPTARG};;
 
         *)
            echo "Incorrect usage"
            usage ;;
     esac
 done
+
+if [ $INSTALLER_TYPE == "apex" ]
+   then
+       if [ -z $APEX_KEY ]
+          then
+              echo "Please provide the  the key to access the APEX Instack VM"
+              usage
+              exit 1
+       fi
+fi
+
+
 ./data/fetch_os_creds.sh -d ./opnfv-creds.sh

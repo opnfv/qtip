94976ad81a823d4a75c10b0139426289888528db
zhifeng.jiang <jiang.zhifeng@zte.com.cn>
2016-07-09 21:25:11 +0800

    Add some UT test cases and fix pep8 errors for driver
    JIRA:QTIP-89
    
    Change-Id: I672ebaf733ec967307963a18f32ca0aa4afc672d
    Signed-off-by: zhifeng.jiang <jiang.zhifeng@zte.com.cn>

--- func/driver.py
+++ func/driver.py
@@ -6,12 +6,10 @@
 # which accompanies this distribution, and is available at
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
-
-
-
 import os
 import json
 from collections import defaultdict
+
 
 class Driver:
 
@@ -21,8 +19,8 @@ class Driver:
         print os.environ['PWD']
         self.dic_json = defaultdict()
 
-    def drive_bench(self, benchmark, roles, benchmark_fname, benchmark_detail = None, pip_dict = None, proxy_info = None):
-        roles= sorted(roles)
+    def drive_bench(self, benchmark, roles, benchmark_fname, benchmark_detail=None, pip_dict=None, proxy_info=None):
+        roles = sorted(roles)
         pip_dict = sorted(pip_dict)
         result_dir = 'results'
         benchmark_name = benchmark + '.yaml'
@@ -33,30 +31,30 @@ class Driver:
         self.dic_json['workingdir'] = str(os.environ['PWD'])
         self.dic_json['fname'] = str(benchmark_fname)
         self.dic_json['username'] = str('root')
-        
+
         for key in proxy_info.keys():
             self.dic_json[key] = proxy_info[key]
-                    
+
         if os.environ['INSTALLER_TYPE'] == str('joid'):
-            self.dic_json['username']=str('ubuntu')
+            self.dic_json['username'] = str('ubuntu')
         if os.environ['INSTALLER_TYPE'] == str('apex'):
-            self.dic_json['username']=str('heat-admin')
-        for k,v in benchmark_detail:
-            self.dic_json[k]=v
+            self.dic_json['username'] = str('heat-admin')
+        for k, v in benchmark_detail:
+            self.dic_json[k] = v
         for k, v in roles:
-            self.dic_json['role']=k
-            index=1
-            if benchmark_detail != None:
+            self.dic_json['role'] = k
+            index = 1
+            if benchmark_detail is not None:
                 for values in v:
-                    if k ==  '1-server':
+                    if k == '1-server':
                         print values, 'saving IP'
-                        self.dic_json['ip'+str(index)]= str(values)
+                        self.dic_json['ip' + str(index)] = str(values)
                         if pip_dict[0][1][0]:
-                            self.dic_json['privateip'+str(index)] = pip_dict[0][1]
+                            self.dic_json['privateip' + str(index)] = pip_dict[0][1]
                         if not pip_dict[0][1][0]:
-                           self.dic_json['privateip'+str(index)] = 'NONE'
-                        index= index+1
+                            self.dic_json['privateip' + str(index)] = 'NONE'
+                        index = index + 1
             dic_json = json.dumps(dict(self.dic_json.items()))
             print dic_json
             run_play = 'ansible-playbook ./benchmarks/playbooks/{0}  --private-key=./data/QtipKey -i ./data/hosts --extra-vars \'{1}\''.format(benchmark_name, dic_json)
-            status = os.system(run_play)
+            os.system(run_play)

80968eefbc126de7e538254bb0cb275a16f005f6
wu.zhihui <wu.zhihui1@zte.com.cn>
2016-09-08 17:38:43 +0800

    Code refactor: setup baremetal/virtual test environment.
    Main code refactor:
    1. args_handler.py: according the type of test case(baremetal/virtual), it will generate different class object(baremetal_setup.Baremetal and virtual_setup)
    2. env_setup.py, baremetal_setup.py, virtual_setup.py: env_setup.Env_setup is parent class of baremetal_setup.Baremetal and virtual_setup.Vitual.
    3. heat/SampleHeat.yaml: update Heat template version and details. It will create own flavor and import Qtip public key.
    4. simplify the code about creating availability zone. It will clean all the aggregates before creating aggregate. 
    5. The Username of VM is "centos" not same with compute node. Make some adaption.
    
    
    Change-Id: Id85b6f82927b41e33504ec6b7c84018030ae7c93
    Signed-off-by: wu.zhihui <wu.zhihui1@zte.com.cn>

---
+++ tests/baremetal_setup_test.py
@@ -0,0 +1,53 @@
+##############################################################################
+# Copyright (c) 2016 ZTE and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+import pytest
+from func.baremetal_setup import Baremetal
+import mock
+
+
+class TestClass:
+
+    @mock.patch("func.env_setup.Env_setup")
+    @mock.patch("func.baremetal_setup.Baremetal.get_host_machine_info")
+    def test_init(self, mock_parent_init, mock_get_host_machine_info):
+        Baremetal()
+        mock_parent_init.assert_called
+        mock_get_host_machine_info.assert_called
+
+    def test_check_machine_ips_without_ip(self):
+        test_class = Baremetal()
+        mock_ips = mock.Mock(return_value=["10.20.0.28", "10.20.0.29"])
+        test_class.fetch_compute_ips = mock_ips
+        inputs = {"machine_1": {"ip": "", "pw": "", "role": "host"},
+                  "machine_2": {"ip": "", "pw": "", "role": "host"}}
+        test_class.check_machine_ips(inputs)
+        assert inputs["machine_1"]['ip'] in ["10.20.0.28", "10.20.0.29"]
+        assert inputs["machine_2"]['ip'] in ["10.20.0.28", "10.20.0.29"]
+        assert inputs["machine_1"]['ip'] != inputs["machine_2"]['ip']
+
+    def test_check_machine_ips_with_ip(self):
+        test_class = Baremetal()
+        mock_ips = mock.Mock(return_value=["10.20.0.28", "10.20.0.29"])
+        test_class.fetch_compute_ips = mock_ips
+        inputs = {"machine_1": {"ip": "10.20.0.28", "pw": "", "role": "host"},
+                  "machine_2": {"ip": "10.20.0.29", "pw": "", "role": "host"}}
+        test_class.check_machine_ips(inputs)
+        assert inputs["machine_1"]['ip'] in ["10.20.0.28", "10.20.0.29"]
+        assert inputs["machine_2"]['ip'] in ["10.20.0.28", "10.20.0.29"]
+        assert inputs["machine_1"]['ip'] != inputs["machine_2"]['ip']
+
+    def test_check_machine_ips_with_invalid_ip(self):
+        test_class = Baremetal()
+        mock_ips = mock.Mock(return_value=["10.20.0.28", "10.20.0.29"])
+        test_class.fetch_compute_ips = mock_ips
+        inputs = {"machine_1": {"ip": "10.20.0.3", "pw": "", "role": "host"},
+                  "machine_2": {"ip": "10.20.0.4", "pw": "", "role": "host"}}
+        with pytest.raises(RuntimeError):
+            test_class.check_machine_ips(inputs)

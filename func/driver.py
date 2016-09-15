8e05bfc1d8065acd0e153327ff90f2040a7a1cde
zhifeng.jiang <jiang.zhifeng@zte.com.cn>
2016-09-15 18:18:14 +0800

    Return the ansible playbook running results in driver.py and args_handler.py
    JIRA:QTIP-99
    
    Change-Id: I875ad52bc69a843e39deb92ad45da90c2e737aae
    Signed-off-by: zhifeng.jiang <jiang.zhifeng@zte.com.cn>

--- func/driver.py
+++ func/driver.py
@@ -7,6 +7,7 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
 import logging
+from operator import add
 from func.ansible_api import AnsibleApi
 
 
@@ -46,7 +47,7 @@ class Driver:
         special_json = {}
         index = roles.index(role) + 1
         special_json.update({'role': role[0]})
-        private_ip = pip_dict[0][1] if pip_dict[0][1][0] else 'NONE'
+        private_ip = pip_dict[0][1][0] if pip_dict[0][1][0] else 'NONE'
         map(lambda x: special_json.update({'ip' + str(index): x}), role[1])\
             if benchmark_detail and (role[0] == '1-server') else None
         map(lambda x: special_json.update({'privateip' + str(index): private_ip}), role[1])\
@@ -59,7 +60,7 @@ class Driver:
         ansible_api.execute_playbook('./data/hosts',
                                      './benchmarks/playbooks/{0}.yaml'.format(benchmark),
                                      './data/QtipKey', extra_vars)
-        return ansible_api.get_detail_playbook_stats()
+        return self.get_ansible_result(extra_vars['role'], ansible_api.get_detail_playbook_stats())
 
     def drive_bench(self, installer_type, pwd, benchmark, roles, benchmark_fname,
                     benchmark_detail=None, pip_dict=None, proxy_info=None):
@@ -67,8 +68,18 @@ class Driver:
         pip_dict = sorted(pip_dict)
         var_json = self.get_common_var_json(installer_type, pwd, benchmark_fname,
                                             benchmark_detail, pip_dict, proxy_info)
-        map(lambda role: self.run_ansible_playbook
-            (benchmark, self.merge_two_dicts(var_json,
-                                             self.get_special_var_json(role, roles,
-                                                                       benchmark_detail,
-                                                                       pip_dict))), roles)
+        result = map(lambda role: self.run_ansible_playbook
+                     (benchmark, self.merge_two_dicts(var_json,
+                                                      self.get_special_var_json(role, roles,
+                                                                                benchmark_detail,
+                                                                                pip_dict))), roles)
+        return reduce(self._merge_ansible_result, result)
+
+    def get_ansible_result(self, role, stats):
+        result = reduce(add, map(lambda x: x[1]['failures'] + x[1]['unreachable'], stats))
+        return {'result': result,
+                'detail': {role: stats}}
+
+    def _merge_ansible_result(self, result_1, result_2):
+        return {'result': result_1['result'] + result_2['result'],
+                'detail': self.merge_two_dicts(result_1['detail'], result_2['detail'])}

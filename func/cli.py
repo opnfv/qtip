e64dd684a778c90f950328a2548bc28da28c1546
linux_geek <taseer94@gmail.com>
2016-09-24 13:30:18 +0500

    Enables to run a single benchmark rather than the whole suite.
    Change-Id: I4cdbbd849117e0bd38583735f5f855f92f4e6bb4
    Signed-off-by: Taseer Ahmed <taseer94@gmail.com>

--- func/cli.py
+++ func/cli.py
@@ -22,37 +22,48 @@ class cli:
                             'on which being tested, These can'
                             'be found in the test_cases/ directory. Please '
                             'ensure that you have edited the respective files '
-                            'before using them. For testing other than through Jenkins'
-                            ' The user should list default after -l . all the fields in'
-                            ' the files are necessary and should be filled')
-        parser.add_argument('-f', '--file', required=True, help='File in '
-                            'test_list with the list of tests. there are three files'
-                            '\n compute '
-                            '\n storage '
-                            '\n network '
-                            'They contain all the tests that will be run. They are listed by suite.'
-                            'Please ensure there are no empty lines')
+                            'before using them. For testing other than through'
+                            'Jenkins. The user should list default after -l.'
+                            'All the fields in the files are necessary and'
+                            'should be filled')
+        parser.add_argument(
+            '-f', '--file', required=True, help='File in '
+            'test_list with the list of tests. there are three'
+            'files'
+            '\n compute '
+            '\n storage '
+            '\n network '
+            'They contain all the tests that will be run. They'
+            'are listed by suite.Please ensure there are no empty lines')
+        parser.add_argument('-b', '--benchmark', required=True, help='Name of the benchmark.'
+                            'Can be found in test_lists/file_name')
+
         return parser.parse_args(args)
 
     def __init__(self, args=sys.argv[1:]):
 
         args = self._parse_args(args)
         if not args_handler.check_suit_in_test_list(args.file):
-            print('\n\n ERROR: Test File Does not exist in test_list/ please enter correct file \n\n')
+            print(
+                '\n\n ERROR: Test File Does not exist in test_list/ please enter correct file \n\n')
             sys.exit(0)
 
         if not args_handler.check_lab_name(args.lab):
-            print('\n\n You have specified a lab that is not present in test_cases/ please enter \
+            print(
+                '\n\n You have specified a lab that is not present in test_cases/ please enter \
                    correct file. If unsure how to proceed, use -l default.\n\n')
             sys.exit(0)
-        suite = args.file
-        benchmarks = args_handler.get_files_in_test_list(suite)
-        test_cases = args_handler.get_files_in_test_case(args.lab, suite)
-        benchmarks_list = filter(lambda x: x in test_cases, benchmarks)
 
-        map(lambda x: args_handler.prepare_and_run_benchmark(
-            os.environ['INSTALLER_TYPE'], os.environ['PWD'],
-            args_handler.get_benchmark_path(args.lab.lower(), suite, x)), benchmarks_list)
-
-        print('{0} is not a Template in the Directory Enter a Valid file name.'
-              'or use qtip.py -h for list'.format(filter(lambda x: x not in test_cases, benchmarks)))
+        if not args_handler.check_benchmark_name(
+                args.lab, args.file, args.benchmark):
+            print('\n\n You have specified an incorrect benchmark. Please enter the correct one.\n\n')
+            sys.exit(0)
+        else:
+            print("Starting with " + args.benchmark)
+            args_handler.prepare_and_run_benchmark(
+                os.environ['INSTALLER_TYPE'],
+                os.environ['PWD'],
+                args_handler.get_benchmark_path(
+                    args.lab.lower(),
+                    args.file,
+                    args.benchmark)) 

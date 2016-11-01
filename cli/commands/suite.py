8fd812eccb09b642c442652f1d9f1feda25e261a
linux_geek <taseer94@gmail.com>
2016-11-01 08:36:46 +0500

    Cli list option for suite
    JIRA: QTIP-135
    
    Change-Id: Icd26e0b04e693b9e16400c25b41df2c1ab9957e5
    Signed-off-by: Taseer Ahmed <taseer94@gmail.com>

---
+++ cli/commands/suite.py
@@ -0,0 +1,52 @@
+##############################################################################
+# Copyright (c) 2016 ZTE Corp and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+from prettytable import PrettyTable
+import os
+import click
+
+
+class Suite:
+
+    def __init__(self):
+        self.path = 'benchmarks/suite/'
+
+    def list(self):
+        table = PrettyTable(["Name"])
+        table.align = 'l'
+        suites = os.listdir(self.path)
+        for suite in suites:
+            table.add_row([suite])
+        click.echo(table)
+
+    def run(self):
+        print("Run a suite")
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.group()
+@click.pass_context
+def suite(ctx):
+    pass
+
+_suite = Suite()
+
+
+@suite.command("list", help="Lists all the available suites")
+def list():
+    _suite.list()
+
+
+@suite.command("run", help="Execute one complete suite")
+def execute():
+    _suite.run()

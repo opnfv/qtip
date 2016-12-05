2176b7dc71da367866bfd40dcf0fde509f001a88
Yujun Zhang <zhang.yujunz@zte.com.cn>
2016-12-05 15:40:02 +0800

    Implement Suite.__init__
    - allow initializing a suite runner with specified root folder
    
    Change-Id: I97a34610d9f50d6ee81b487d0446eec3f4eea001
    Signed-off-by: Yujun Zhang <zhang.yujunz@zte.com.cn>

--- tests/unit/runner/suite_test.py
+++ tests/unit/runner/suite_test.py
@@ -7,7 +7,21 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
 
+from os import path
+
+from qtip.runner.suite import Suite
+
 
 class TestSuite:
+    root = path.join(path.dirname(__file__), path.pardir, path.pardir,
+                     'data')
+
+    def test_init(self):
+        suite = Suite()
+        assert suite.root.endswith('benchmarks')
+
+        suite = Suite(root=self.root)
+        assert suite.root.endswith(path.join('data'))
+
     def test_list(self):
         assert True

ab5e5f5d602c4e5c84f99c2b5ba3b97d3379f5cf
Yujun Zhang <zhang.yujunz@zte.com.cn>
2016-12-05 15:40:02 +0800

    Implement __init__ and list_all in Suite
    Change-Id: I97a34610d9f50d6ee81b487d0446eec3f4eea001
    Signed-off-by: Yujun Zhang <zhang.yujunz@zte.com.cn>

--- tests/unit/runner/suite_test.py
+++ tests/unit/runner/suite_test.py
@@ -7,7 +7,35 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
 
+from os import path
+import pytest
+
+from qtip.runner.suite import Suite
+from qtip.runner.suite import SuiteProperty as SProp
+
+
+class TestSuiteClass:
+    def test_attr(self):
+        assert len(Suite._paths) is 1
+
 
 class TestSuite:
+    Suite._paths = [path.join(path.dirname(__file__), path.pardir, path.pardir,
+                              'data', 'suite')]
+
+    def test_init(self):
+        suite = Suite('suite-1')
+        assert suite.name == 'suite-1'
+
+        with pytest.raises(TypeError) as excinfo:
+            Suite()
+        assert '__init__() takes exactly 2 arguments (1 given)' \
+               in str(excinfo.value)
+
     def test_list(self):
-        assert True
+        suite_list = Suite.list_all()
+        assert len(list(suite_list)) is 3
+        for suite_desc in suite_list:
+            assert SProp.NAME in suite_desc
+            assert SProp.DESCRIPTION in suite_desc
+            assert SProp.ABSPATH in suite_desc

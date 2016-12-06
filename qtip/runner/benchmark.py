89e3f17d136f80c5406cdaff67d33b4c8159aac4
Yujun Zhang <zhang.yujunz@zte.com.cn>
2016-12-06 16:30:11 +0800

    Implement TestPlan prototype
    Refactor common part of Suite to Benchmark since both TestPlan
    and Suite are organized by files
    
    Change-Id: I61a97d9489096c4a6305c99e8cf7abb958faa562
    Signed-off-by: Yujun Zhang <zhang.yujunz@zte.com.cn>

---
+++ qtip/runner/benchmark.py
@@ -0,0 +1,56 @@
+##############################################################################
+# Copyright (c) 2016 ZTE Corp and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+from itertools import chain
+from os import listdir
+from os import path
+
+
+class Property:
+    NAME = 'name'
+    DESCRIPTION = 'description'
+    ABSPATH = 'abspath'
+
+
+class Benchmark:
+    """Abstract class of QTIP benchmarks"""
+
+    # paths to search for suites
+    _paths = [path.join(path.dirname(__file__), path.pardir, path.pardir,
+                        'benchmarks')]
+
+    def __init__(self, name):
+        """:param name: suite name"""
+        # TODO(yujunz) check existence and expand to full path
+        self.name = name
+        self._abspath = self._find(name)
+
+    def _find(self, name):
+        """find a benchmark in searching paths"""
+        for p in self._paths:
+            abspath = path.join(p, name)
+            if path.exists(abspath):
+                return abspath
+        return None
+
+    @classmethod
+    def list_all(cls):
+        """list all available benchmarks"""
+        names = chain.from_iterable([listdir(p) for p in cls._paths])
+        return [Benchmark(name).describe() for name in names]
+
+    def describe(self):
+        """description of benchmark"""
+        # TODO(yujunz)
+        # - read description from benchmark content
+        return {
+            Property.NAME: self.name,
+            Property.DESCRIPTION: 'QTIP benchmark',
+            Property.ABSPATH: self._abspath
+        }

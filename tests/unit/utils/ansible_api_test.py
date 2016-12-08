027ed34df5a6507a4d7460ed72f307c38b528ead
Yujun Zhang <zhang.yujunz@zte.com.cn>
2016-12-08 14:19:26 +0800

    Fix relative path in test case
    Use abspath for fixture files to make tests independent from cwd
    
    Change-Id: Ie965899c5311e26ad708f44463b1992d9b8e697b
    Signed-off-by: Yujun Zhang <zhang.yujunz@zte.com.cn>

--- tests/unit/utils/ansible_api_test.py
+++ tests/unit/utils/ansible_api_test.py
@@ -6,14 +6,17 @@
 # which accompanies this distribution, and is available at
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
+
+from tests import Fixture
 from qtip.utils.ansible_api import AnsibleApi
 
 
 class TestClass:
+
     def test_call_ansible_api_success(self):
         ansible_api = AnsibleApi()
-        ret = ansible_api.execute_playbook('tests/data/hosts',
-                                           'tests/data/test.yml',
-                                           'config/QtipKey',
+        ret = ansible_api.execute_playbook(Fixture.abspath('hosts'),
+                                           Fixture.abspath('test.yml'),
+                                           Fixture.abspath('QtipKey'),
                                            {'keys': 'test'})
         assert ret == 3

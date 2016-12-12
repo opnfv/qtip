8df46f59c0814a1abe90458eae3e8c58a8a17c04
Taseer <taseer94@gmail.com>
2016-12-13 00:47:46 +0500

    Basic testplan layout JIRA: QTIP-184
    Change-Id: I605f81fce9c4c59b75d286a6262995ea2fa2e21e
    Signed-off-by: Taseer Ahmed <taseer94@gmail.com>

---
+++ qtip/cli/commands/cmd_testplan.py
@@ -0,0 +1,36 @@
+#############################################################################
+# Copyright (c) 2016 ZTE Corp and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+import click
+
+from qtip.runner.testplan import TestPlan
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.group()
+def testplan():
+    pass
+
+
+_testplan = TestPlan('compute')
+
+
+@testplan.command('list', help='List the different tesplans.')
+def list():
+    pass
+
+
+@testplan.command('show', help='Show details of specified plan.')
+@click.argument('testplan_name')
+def show(testplan_name):
+    pass

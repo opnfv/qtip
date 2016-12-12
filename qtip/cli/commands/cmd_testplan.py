5fdc5666dad7d2fed64983ef8b40400871de390f
Taseer <taseer94@gmail.com>
2016-12-13 00:47:46 +0500

    Basic testplan layout JIRA: QTIP-184
    Change-Id: I605f81fce9c4c59b75d286a6262995ea2fa2e21e
    Signed-off-by: Taseer Ahmed <taseer94@gmail.com>

---
+++ qtip/cli/commands/cmd_testplan.py
@@ -0,0 +1,43 @@
+#############################################################################
+# Copyright (c) 2016 ZTE Corp and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+import click
+from prettytable import PrettyTable
+from qtip.runner.testplan import TestPlan
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.group()
+def testplan():
+    pass
+
+
+@testplan.command('list', help='List the different TestPlans.')
+def list():
+    testplans = TestPlan.list_all()
+    table = PrettyTable(["Testplans"])
+    table.align = 'l'
+    for testplan in testplans:
+        table.add_row([testplan['name']])
+    click.echo(table)
+
+
+@testplan.command('show', help='Show details of specified TestPlan.')
+@click.argument('name')
+def show(name):
+    plan = TestPlan(name)
+    results = plan.describe()
+    table = PrettyTable(["Name", "Description"])
+    table.align = 'l'
+    table.add_row([results['name'], results['description']])
+    click.echo(table)

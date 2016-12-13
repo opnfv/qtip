af0f44a8a19a902362f4a720aa6cc495eece1362
Taseer <taseer94@gmail.com>
2016-12-14 00:00:06 +0500

    Result module outline.
    JIRA: QTIP-185
    
    Change-Id: I2d40ac587d26d1784644521e9438ffebd1ed1b58
    Signed-off-by: Taseer Ahmed <taseer94@gmail.com>

---
+++ qtip/cli/commands/cmd_result.py
@@ -0,0 +1,56 @@
+##############################################################################
+# Copyright (c) 2016 ZTE Corp and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+import click
+from prettytable import PrettyTable
+
+
+class Result:
+
+    def __init__(self):
+        pass
+
+    def list(self):
+        click.echo("List")
+
+    def node(self):
+        click.echo("Node names")
+
+    def view(self, name, node):
+        click.echo("Showing: %s " % name + " on %s " % node)
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.group()
+def result():
+    pass
+
+
+_result = Result()
+
+
+@result.command("list", help="List all PerfTests carried out")
+def list():
+    _result.list()
+
+
+@result.command("node", help="List all the compute nodes in cluster")
+def node():
+    _result.node()
+
+
+@result.command("view", help="View the result of specified PerfTest")
+@click.argument("name")
+@click.argument("node")
+def view(name, node):
+    _result.view(name, node)

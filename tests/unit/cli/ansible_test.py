0f8c865bbebab462c3557f5f5971b4d5496d40d2
Taseer <taseer94@gmail.com>
2016-12-15 22:43:05 +0500

    Unit Test for Ansible Module
    JIRA: QTIP-188
    
    Change-Id: I10b3fb979f52d10e8dedf956831f8320c04a36ea
    Signed-off-by: Taseer Ahmed <taseer94@gmail.com>

---
+++ tests/unit/cli/ansible_test.py
@@ -0,0 +1,31 @@
+##############################################################################
+# Copyright (c) 2016 ZTE Corp and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+import pytest
+from click.testing import CliRunner
+from qtip.cli.entry import cli
+
+
+class TestClass(object):
+
+    @pytest.fixture()
+    def runner(self):
+        return CliRunner()
+
+    def test_prepare(self, runner):
+        result = runner.invoke(cli, ['ansible', 'prepare'])
+        assert result.output == "prepare ansible env\n"
+
+    def test_show(self, runner):
+        result = runner.invoke(cli, ['ansible', 'show'])
+        assert result.output == "show ansible configuration\n"
+
+    def test_status(self, runner):
+        result = runner.invoke(cli, ['ansible', 'status'])
+        assert result.output == "check connectivity\n"

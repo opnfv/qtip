802994d46f25f6427bb0d797aaabb94b427827ef
Taseer <taseer94@gmail.com>
2016-12-20 22:27:05 +0500

    All unit tests.
    JIRA: QTIP-184
    
    Change-Id: I577b8df85d2be63f9195ed2bb3c6bf497ce1fd96
    Signed-off-by: Taseer Ahmed <taseer94@gmail.com>

---
+++ tests/unit/cli/test_module.py
@@ -0,0 +1,30 @@
+import os
+import pytest
+import sys
+import yaml
+
+from click.testing import CliRunner
+from qtip.cli.entry import cli
+
+
+class TestClass(object):
+
+    @pytest.fixture()
+    def runner(self):
+        return CliRunner()
+
+    def test(self, runner):
+        unit = 'ansible'
+        test_file = 'data/helper/' + unit + '.yaml'
+        path = os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, test_file)
+
+        with open(path) as trial:
+            content = trial.read()
+            data = yaml.safe_load(content)['tests']
+            if data is None:
+                print("Unit Test does not exist")
+                sys.exit(1)
+            else:
+                for i in range(0, len(data)):
+                    result = runner.invoke(cli, data[i]['command'])
+                    assert result.output == data[i]['output']

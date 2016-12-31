80ffdc589a76b12d196272556a39b315d4ceb670
Yujun Zhang <zhang.yujunz@zte.com.cn>
2016-12-31 21:27:38 +0800

    sample QPI for doctor performance measured in verification job
    The doctor benchmark plan produces a performance profile consist of
    timestamp at each checkpoint and time cost of each phase.[1]
    
    It collects data from log files and render a timeline diagram in console
    
    Current progress
    
    - skeleton created
    - method to be implemented
    
    [1]: http://artifacts.opnfv.org/doctor/docs/design/index.html#document-performance-profiler
    
    Change-Id: I60fbbd37d04a5724841bd85cb3bfed9eabd3741a
    Signed-off-by: Yujun Zhang <zhang.yujunz@zte.com.cn>

---
+++ qtip/collector/transformer/timeline.py
@@ -0,0 +1,14 @@
+##############################################################################
+# Copyright (c) 2016 ZTE Corp and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+from base import BaseTransformer
+
+
+class Timeline(BaseTransformer):
+    """transform collected metrics into object required by timeline template"""

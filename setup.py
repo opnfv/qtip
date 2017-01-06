04cc3fbe59174e9a5362c753e1b18710dbc7da79
srisankethu <iamjustice443@gmail.com>
2017-01-06 19:23:07 +0530

    setup.py: Add install_requires
    Added install_requires to install non-conflicting requirements
    
    Change-Id: Ib7474680bebbb4ef1085748ffeaf055599aade90
    Signed-off-by: srisankethu <iamjustice443@gmail.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,10 @@
 
 from setuptools import setup
 
+requirements = open('requirements.txt')
+
 setup(
     setup_requires=['pbr>=1.9', 'setuptools>=17.1'],
     pbr=True,
+    install_requires = requirements.read().splitlines(),
 )

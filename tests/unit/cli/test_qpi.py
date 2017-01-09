4efee6867afb23659bb4eae84a139f50ed34f307
Taseer Ahmed <taseer94@gmail.com>
2017-01-09 20:53:37 +0500

    QPI Module Outline
    Change-Id: I19b666dfdb3e3c10622397bda00460d6fb5c269d
    Signed-off-by: Taseer Ahmed <taseer94@gmail.com>

---
+++ tests/unit/cli/test_qpi.py
@@ -0,0 +1,28 @@
+###############################################################
+# Copyright (c) 2016 ZTE Corp and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+import pytest
+from click.testing import CliRunner
+
+from qtip.cli.commands.cmd_qpi import cli
+
+
+@pytest.fixture()
+def runner():
+    return CliRunner()
+
+
+def test_list(runner):
+    result = runner.invoke(cli, ['list'])
+    assert result.output == ''
+
+
+def test_run(runner):
+    result = runner.invoke(cli, ['run', 'dhrystone'])
+    assert result.output == ''

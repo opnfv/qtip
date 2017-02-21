3e443dff14a2be02b914e66f27b549d0ed4cc600
Yujun Zhang <zhang.yujunz@zte.com.cn>
2017-02-21 15:22:44 +0800

    Add licence header according to OPNFV contribution guidelines[1] by script[2]
    [1]: https://wiki.opnfv.org/display/DEV/Contribution+Guidelines
    [2]: https://github.com/Justin-chi/Lab/blob/master/add_license.sh
    
    Change-Id: I5b77394a104857bb4ee9addbe973ed239b7a59f3
    Signed-off-by: Yujun Zhang <zhang.yujunz@zte.com.cn>

--- legacy/scripts/ref_results/compute_benchmarks_indices.py
+++ legacy/scripts/ref_results/compute_benchmarks_indices.py
@@ -1,3 +1,11 @@
+##############################################################################
+# Copyright (c) 2017 ZTE Corporation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
 from index_calculation import generic_index as get_index
 from index_calculation import get_reference
 from result_accum import result_concat as concat

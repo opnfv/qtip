119084e99e555ed1f16c18648fd8aca7d70cc7d1
SerenaFeng <feng.xiaowei@zte.com.cn>
2017-02-22 17:31:10 +0800

    add logger's unittest
    Change-Id: I9a7d3c6afeff5432017f5bfd4a961a719c93c086
    Signed-off-by: SerenaFeng <feng.xiaowei@zte.com.cn>

--- qtip/util/logger.py
+++ qtip/util/logger.py
@@ -27,19 +27,19 @@ import os
 
 
 class Logger(object):
-    file_path = '/var/log'
     formatter = logging.Formatter('%(asctime)s - %(name)s - '
                                   '%(levelname)s - %(message)s')
-
-    def __init__(self, logger_name):
-
+    
+    def __init__(self, logger_name, file_path=None):
+        self.file_path = '/var/log' if not file_path else file_path
+        
         IF_DEBUG = os.getenv('IF_DEBUG')
-
+        
         self.logger_name = logger_name
         self.logger = logging.getLogger(logger_name)
         self.logger.propagate = 0
         self.logger.setLevel(logging.DEBUG)
-
+        
         ch = logging.StreamHandler()
         ch.setFormatter(self.formatter)
         if IF_DEBUG is not None and IF_DEBUG.lower() == "true":
@@ -47,22 +47,21 @@ class Logger(object):
         else:
             ch.setLevel(logging.INFO)
         self.logger.addHandler(ch)
-
+        
         hdlr = logging.FileHandler('%s/%s.log' % (self.file_path, logger_name))
         hdlr.setFormatter(self.formatter)
         hdlr.setLevel(logging.DEBUG)
         self.logger.addHandler(hdlr)
-
+    
     @property
     def get(self):
         return self.logger
 
 
 class QtipLogger(Logger):
-    file_path = '{}/qtip/logs'.format(os.environ['HOME'])
-
     def __init__(self, logger_name):
+        self.file_path = '{}/qtip/logs'.format(os.environ['HOME'])
         if not os.path.exists(self.file_path):
             os.makedirs(self.file_path)
-
-        super(QtipLogger, self).__init__(logger_name)
+        
+        super(QtipLogger, self).__init__(logger_name, self.file_path)

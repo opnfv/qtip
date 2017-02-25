fde22e02eca2008204da1ce3b16b4991dbc228fc
Taseer Ahmed <taseer94@gmail.com>
2017-02-25 10:15:57 +0500

    Adding color support for log output on console. JIRA: QTIP-216
    Change-Id: I517972cb0d8fd2852e7aad5fe987d6905a3b50ac
    Signed-off-by: Taseer Ahmed <taseer94@gmail.com>

---
+++ qtip/util/colors.py
@@ -0,0 +1,18 @@
+##############################################################################
+# Copyright (c) 2017 ZTE Corporation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+
+class Colors:
+
+    DEBUG = '\033[94m'
+    INFO = '\033[92m'
+    WARN = '\033[93m'
+    ERROR = '\033[91m'
+    CRITICAL = '\033[95m'
+    NORMAL = '\033[0m'

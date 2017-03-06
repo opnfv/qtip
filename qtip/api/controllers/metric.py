2d98a7d6064a196d65ef78b9df6be362f89a61b1
akhilbatra898 <akhil.batra@research.iiit.ac.in>
2017-03-06 05:45:54 +0530

    Add basic swagger specifications and corresponding controllers
    - Update specifications as per requirements
    - map specifications with controllers
    - make api installable
    
    JIRA: QTIP-220
    
    Change-Id: Id149fdcf68e869e31a00cf16d7e725e368d2b25f
    Signed-off-by: akhilbatra898 <akhil.batra@research.iiit.ac.in>

--- qtip/api/controllers/metric.py
+++ qtip/api/controllers/metric.py
@@ -8,17 +8,15 @@
 ##############################################################################
 
 import connexion
-import os
 
 
-swagger_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'swagger/'))
+def list_metrics():
+    return connexion.problem(501,
+                             'List metrics',
+                             'Metrics listing not implemented')
 
 
-def main():
-    app = connexion.App(__name__, specification_dir=swagger_dir)
-    app.add_api('swagger.yaml', base_path='/v1.0')
-    app.run(host='0.0.0.0', port='5000')
-
-
-if __name__ == '__main__':
-    main()
+def get_metric(metric_name):
+    return connexion.problem(501,
+                             'Get a metric',
+                             'metric retrieval not implemented')

b43d4a358393f0a0c86b6ddec9c2ae5a056024aa
Taseer Ahmed <taseer94@gmail.com>
2017-03-07 00:21:37 +0500

    Qtip component listing.
    - Load components from different paths
    
    JIRA: QTIP-205
    
    Change-Id: Ic5df7d8ea99577947bdfe15be133cdbfb7beb8dd
    Signed-off-by: Taseer Ahmed <taseer94@gmail.com>

--- tests/unit/cli/cmd_qpi_test.py
+++ tests/unit/cli/cmd_qpi_test.py
@@ -1,5 +1,5 @@
 ###############################################################
-# Copyright (c) 2016 ZTE Corp and others.
+# Copyright (c) 2017 taseer94@gmail.com and others.
 #
 # All rights reserved. This program and the accompanying materials
 # are made available under the terms of the Apache License, Version 2.0
@@ -20,7 +20,7 @@ def runner():
 
 def test_list(runner):
     result = runner.invoke(cli, ['qpi', 'list'])
-    assert result.output == ''
+    assert 'compute' and 'fake-qpi' in result.output
 
 
 def test_run(runner):

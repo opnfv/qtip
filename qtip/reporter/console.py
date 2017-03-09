3eb389001c92fe8e22f66970f98cff5ca8a8ca04
Taseer <taseer94@gmail.com>
2017-03-09 12:05:46 +0500

    Refactor reporter module
    - Create templates for report format specific to each metric.
    - Outputs the details of the specified metric across all the nodes
      in cluster.
    - Timeline to be covered in a separate patch
    
    JIRA: QTIP-199
    
    Change-Id: Ic83749725b0c9cc5bd9a7f24f21b2cd113abe0e1
    Signed-off-by: Taseer Ahmed <taseer94@gmail.com>

--- qtip/reporter/console.py
+++ qtip/reporter/console.py
@@ -7,24 +7,36 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
 
+import json
+
 from jinja2 import Environment
 from jinja2 import FileSystemLoader
 from os import path
 from qtip.base import BaseActor
 
+ROOT_DIR = path.join(path.dirname(__file__), path.pardir, path.pardir)
+
 
 class ConsoleReporter(BaseActor):
-    """
-    report benchmark result to console
-    """
+    """ report benchmark result to console """
+
     def __init__(self, config, parent=None):
         super(ConsoleReporter, self).__init__(config, parent=parent)
 
         # TODO (taseer) load template from config
-        tpl_loader = FileSystemLoader(path.join(path.dirname(__file__), 'templates'))
-        env = Environment(loader=tpl_loader)
-        self._template = env.get_template('timeline.j2')
+        tpl_path = path.join(path.dirname(__file__), 'templates')
+        tpl_loader = FileSystemLoader(tpl_path)
+        self._env = Environment(loader=tpl_loader)
+        self.result_path = path.join(ROOT_DIR, 'collector')
 
-    def render(self, var_dict):
-        out = self._template.render(var_dict)
+    def load_result(self):
+        with open(path.join(self.result_path, 'result.json')) as sample:
+            result = json.load(sample)
+        return result
+
+    def render(self, metric):
+        template = self._env.get_template('base.j2')
+        var_dict = self.load_result()
+        var_dict['metric_name'] = metric
+        out = template.render(var_dict)
         return out

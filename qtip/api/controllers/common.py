931ca3dd23940613a4ec146447d3c2d297d9bed0
akhilbatra898 <akhil.batra@research.iiit.ac.in>
2017-03-18 14:16:26 +0530

    Add unit tests for List and get in API.
     - refactor controllers
     - remove abspath and other irrelvant data in response
     - move fixtures
     - refactor decorators
    
    JIRA: QTIP-226
    
    Change-Id: I5fac5b1bc998da198098992e7ddb47ba49685f31
    Signed-off-by: akhilbatra898 <akhil.batra@research.iiit.ac.in>

--- qtip/api/controllers/common.py
+++ qtip/api/controllers/common.py
@@ -1,3 +1,12 @@
+##############################################################################
+# Copyright (c) 2017 ZTE Corporation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
 import httplib
 
 import connexion
@@ -5,15 +14,22 @@ import connexion
 from qtip.base import error
 
 
-def get_one_exceptions(resource):
+def check_endpoint_for_error(resource, operation=None):
     def _decorator(func):
-        def _execute(name):
+        def _execute(name=None):
             try:
                 return func(name), httplib.OK
             except error.NotFoundError:
                 return connexion.problem(
                     httplib.NOT_FOUND,
-                    '{} Not Found'.format(resource),
-                    'Requested {} `{}` not found.'.format(resource, name))
+                    '{} not found'.format(resource),
+                    'Requested {} `{}` not found.'
+                    .format(resource.lower(), name))
+            except error.ToBeDoneError:
+                return connexion.problem(
+                    httplib.NOT_IMPLEMENTED,
+                    '{} handler not implemented'.format(operation),
+                    'Requested operation `{}` on {} not implemented.'
+                    .format(operation.lower(), resource.lower()))
         return _execute
     return _decorator

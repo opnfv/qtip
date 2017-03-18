ad59254f5b60f9555d2bb3e0a2968165deafda43
akhilbatra898 <akhil.batra@research.iiit.ac.in>
2017-03-18 14:16:26 +0530

    Add unit tests for List and get in API.
     - refactor controllers
     - remove abspath and other irrelvant data in response
     - move fixtures
     - complete decorators
    
    JIRA: QTIP-226
    
    Change-Id: I5fac5b1bc998da198098992e7ddb47ba49685f31
    Signed-off-by: akhilbatra898 <akhil.batra@research.iiit.ac.in>

--- qtip/api/controllers/common.py
+++ qtip/api/controllers/common.py
@@ -1,3 +1,12 @@
+##############################################################################
+# Copyright (c) 2017 akhil.batra@research.iiit.ac.in and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
 import httplib
 
 import connexion
@@ -5,7 +14,7 @@ import connexion
 from qtip.base import error
 
 
-def get_one_exceptions(resource):
+def check_resource(resource):
     def _decorator(func):
         def _execute(name):
             try:
@@ -13,7 +22,21 @@ def get_one_exceptions(resource):
             except error.NotFoundError:
                 return connexion.problem(
                     httplib.NOT_FOUND,
-                    '{} Not Found'.format(resource),
-                    'Requested {} `{}` not found.'.format(resource, name))
+                    '{} not found'.format(resource),
+                    'Requested {} `{}` not found.'.format(resource.lower(), name))
+        return _execute
+    return _decorator
+
+
+def check_implemented(resource, operation):
+    def _decorator(func):
+        def _execute(name):
+            try:
+                return func(name), httplib.OK
+            except error.ToBeDoneError:
+                return connexion.problem(
+                    httplib.NOT_IMPLEMENTED,
+                    '{} handler not implemented'.format(operation),
+                    'Requested operation `{}` on {} not implemented.'.format(operation.lower(), resource.lower()))
         return _execute
     return _decorator

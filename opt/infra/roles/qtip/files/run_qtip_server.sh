05d5e8a5baca44033ebf7213e32a99bebc17febe
Julien <zhang.jun3g@zte.com.cn>
2017-03-23 10:37:07 +0800

    Fix socker connection issue in qtip docker
    ansible can not run correctly in qtip docker currently in centos.
    
    Change-Id: I92875924ae8047e20d1bb73ad655b8b8f6a59523
    Signed-off-by: Julien <zhang.jun3g@zte.com.cn>
    (cherry picked from commit 899840fd5e7b0a6cde1d5f19a7725c5362e0cb26)

--- opt/infra/roles/qtip/files/run_qtip_server.sh
+++ opt/infra/roles/qtip/files/run_qtip_server.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
 
 envs="INSTALLER_TYPE=fuel -e INSTALLER_IP=10.20.0.2 -e NODE_NAME=zte-pod1"
-docker run --name qtip -id -e $envs -p 5000:5000 opnfv/qtip
+
+# use ramfs to fix docker socket connection issue with overlay mode in centos
+ramfs=/tmp/qtip/ramfs
+if [ ! -d $ramfs ]; then
+    mkdir -p $ramfs
+fi
+
+if [ ! -z $(df $ramfs | tail -n -1 | grep $ramfs) ]; then
+    sudo mount -t tmpfs -o size=32M tmpfs $ramfs
+fi
+
+# enable contro path in docker
+echo <<EOF > /tmp/ansible.cfg
+[defaults]
+callback_whitelist = profile_tasks
+[ssh_connection]
+control_path=/mnt/ramfs/ansible-ssh-%%h-%%p-%%r
+EOF
+
+docker run --name qtip -id -e $envs -p 5000:5000 -v $ramfs:/mnt/ramfs opnfv/qtip
+docker cp qtip /tmp/ansible.cfg /home/opnfv/.ansible.cfg

6bec7f3a38497222217cf899965bc6d4d5c0c545
Yujun Zhang <zhang.yujunz@zte.com.cn>
2017-04-17 22:05:56 +0800

    Aggregate qpi score from all nodes
    Change-Id: Ib711a493a949b013ffe22519861f144dc47d0334
    Signed-off-by: Yujun Zhang <zhang.yujunz@zte.com.cn>

---
+++ qtip/ansible_library/plugins/action/aggregate.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+###############################################################
+# Copyright (c) 2017 ZTE Corporation
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+from numpy import mean
+
+from ansible.plugins.action import ActionBase
+
+
+class ActionModule(ActionBase):
+    def run(self, tmp=None, task_vars=None):
+
+        if task_vars is None:
+            task_vars = dict()
+
+        result = super(ActionModule, self).run(tmp, task_vars)
+
+        if result.get('skipped', False):
+            return result
+
+        return aggregate(self._task.args.get('group'), task_vars)
+
+
+# aggregate QPI results
+def aggregate(group, task_vars):
+    qpi_results = [task_vars['hostvars'][host]['qpi_result'] for host in task_vars['groups'][group]]
+    return {
+        'score': mean([r['score'] for r in qpi_results])
+    }

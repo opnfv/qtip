64f49db74e5e44439cc8aec235d0ac1888dc09af
Taseer <taseer94@gmail.com>
2017-04-25 17:18:27 +0500

    (WIP) Update CLI according to new architecture
    Change-Id: Ibd4213a6c4c86a7f4e8f99b16cba5055a3abba39
    Signed-off-by: Taseer Ahmed <taseer94@gmail.com>

---
+++ qtip/cli/commands/cmd_workspace.py
@@ -0,0 +1,29 @@
+##############################################################################
+# Copyright (c) 2017 taseer94@gmail.com and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+
+import click
+import os
+from os import path
+
+
+roles_path = path.join(path.dirname(__file__), os.pardir, os.pardir, os.pardir,
+                      "resources/ansible_roles")
+
+
+@click.group()
+def cli():
+    """ Manage QTIP workspace """
+    pass
+
+
+@cli.command("create", help="Create QTIP workspace")
+def create():
+    os.system("ANSIBLE_ROLES_PATH={0} ansible_playbook"
+              "{1}/workspace-create.yml".format(roles_path, roles_path))

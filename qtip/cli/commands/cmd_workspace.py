9163493b4a3040197b90790169542b438291e74b
Taseer <taseer94@gmail.com>
2017-04-25 17:18:27 +0500

    Update CLI according to new architecture
    - Implement workspace generation
    - Implement setup/run in separate patch
    
    Change-Id: Ibd4213a6c4c86a7f4e8f99b16cba5055a3abba39
    Signed-off-by: Taseer Ahmed <taseer94@gmail.com>

---
+++ qtip/cli/commands/cmd_workspace.py
@@ -0,0 +1,27 @@
+##############################################################################
+# Copyright (c) 2017 taseer94@gmail.com and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+
+import click
+import os
+
+from qtip.cli import utils
+
+
+@click.group()
+def cli():
+    """ Manage QTIP workspace """
+    pass
+
+
+@cli.command("create", help="Create QTIP workspace")
+def create():
+    os.system("ANSIBLE_ROLES_PATH={0} ansible-playbook "
+              "{1}/qtip-workspace/create.yml --extra-vars 'qtip_package=../../../..'"
+              "".format(utils.roles_path, utils.roles_path))

484ed376614b57e9ea8798ab8c713756c0eb8cc8
Taseer <taseer94@gmail.com>
2017-06-05 20:57:57 +0500

    (WIP) Integrate Runner with CLI
    - Deprecate Jinja Templates. Too specific
    - Render templates programmatically
    
    Change-Id: Ia5d1c263413b0a8886dbc083e2b3e3b194da8848
    Signed-off-by: Taseer Ahmed <taseer94@gmail.com>

--- qtip/cli/commands/cmd_report.py
+++ qtip/cli/commands/cmd_report.py
@@ -7,26 +7,51 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
 
+from asq.initiators import query
 import click
+from prettytable import PrettyTable
 
-from qtip.cli.entry import Context
 from qtip.reporter.console import ConsoleReporter
 
-pass_context = click.make_pass_decorator(Context, ensure=False)
+
+def extract_workload(section, section_name):
+    """ Extract information related to SPEC and BASELINE """
+    wl = query(section['sections']).where(lambda sectn: sectn['name'] == section_name) \
+                                   .select(lambda sectn: sectn['metrics']) \
+                                   .to_list()
+    return wl
+
+
+def extract_score(section, section_name, node):
+    """ Extract information related to QPI """
+    qpi = query(section['children']).where(lambda child: child['name'] == node) \
+                                    .select_many(lambda child: child['children']) \
+                                    .where(lambda child: child['name'] == section_name) \
+                                    .to_list()
+    return qpi
 
 
 @click.group()
-@pass_context
-def cli(ctx):
+def cli():
     """ View QTIP results"""
     pass
 
 
 @cli.command('show')
-@click.argument('metric')
-@click.option('-p', '--path', help='Path to result directory')
-@pass_context
-def show(ctx, metric, path):
+@click.option('-n', '--node', help="Compute node in OPNFV cluster")
+@click.argument('section-name')
+def show(node, section_name):
     reporter = ConsoleReporter({})
-    report = reporter.render(metric, path)
-    click.echo(report)
+    report = reporter.render()
+    table_workload = PrettyTable(['workload', 'Description', 'Baseline', 'Results', 'Score'])
+    table_workload.align = 'l'
+
+    spec = extract_workload(report[0], section_name)
+    baseline = extract_workload(report[1], section_name)
+    scores = extract_score(report[2], section_name, node)
+
+    for sp, bl, score in zip(spec[0][0]['workloads'], baseline[0][0]['workloads'],
+                             scores[0]['children'][0]['children']):
+        table_workload.add_row([sp['name'], sp['description'], bl['baseline'],
+                               1, score['score']])
+    click.echo(table_workload)

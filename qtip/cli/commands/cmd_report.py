92c383b762c13ee43897f9cb3caaaf3c42d04163
Taseer <taseer94@gmail.com>
2017-06-05 20:57:57 +0500

    (WIP) Integrate with CLI
    - Deprecate Jinja Templates. Too specific
    - Render templates programmatically
    
    Change-Id: Ia5d1c263413b0a8886dbc083e2b3e3b194da8848
    Signed-off-by: Taseer Ahmed <taseer94@gmail.com>

--- qtip/cli/commands/cmd_report.py
+++ qtip/cli/commands/cmd_report.py
@@ -8,25 +8,49 @@
 ##############################################################################
 
 import click
+from prettytable import PrettyTable
 
-from qtip.cli.entry import Context
 from qtip.reporter.console import ConsoleReporter
 
-pass_context = click.make_pass_decorator(Context, ensure=False)
+
+def get_val(param, metric):
+    result = []
+    for section in param['sections']:
+        if section['name'] == metric:
+            for metric in section['metrics']:
+                result.append(metric['workloads'])
+    return result
+
+
+def get_result(param, metric, node):
+    for child in param['children']:
+        if child['name'] == node:
+            for child_metric in child['children']:
+                if child_metric['name'] == metric:
+                    return child_metric['children']
 
 
 @click.group()
-@pass_context
-def cli(ctx):
+def cli():
     """ View QTIP results"""
     pass
 
 
 @cli.command('show')
 @click.argument('metric')
-@click.option('-p', '--path', help='Path to result directory')
-@pass_context
-def show(ctx, metric, path):
+@click.argument('node')
+def show(metric, node):
     reporter = ConsoleReporter({})
-    report = reporter.render(metric, path)
-    click.echo(report)
+    report = reporter.render()
+    table_workload = PrettyTable(['workload', 'Description', 'Baseline', 'Results', 'Score'])
+    table_workload.align = 'l'
+
+    spec = get_val(report[0], metric)
+    baseline = get_val(report[1], metric)
+    qpi = get_result(report[2], metric, node)
+
+    for i in range(0, len(spec)):
+        for j in range(0, len(spec[i])):
+            table_workload.add_row([spec[i][j]['name'], spec[i][j]['description'], baseline[i][j]['baseline'],
+                                   1, qpi[0]['children'][j]['score']])
+    click.echo(table_workload)

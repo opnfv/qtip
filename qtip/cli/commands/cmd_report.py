25d9c2baab4ac4abdef3d95b4da1203d45da924c
Taseer <taseer94@gmail.com>
2017-06-07 21:48:04 +0500

    Integrate Report and CLI
    JIRA: QTIP-244
    
    Change-Id: I70b2b2a4a1a7a98f857ca74ead0c4fcd558e7168
    Signed-off-by: Taseer Ahmed <taseer94@gmail.com>

--- qtip/cli/commands/cmd_report.py
+++ qtip/cli/commands/cmd_report.py
@@ -7,26 +7,54 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
 
+from asq.initiators import query
 import click
+from prettytable import PrettyTable
 
-from qtip.cli.entry import Context
 from qtip.reporter.console import ConsoleReporter
 
-pass_context = click.make_pass_decorator(Context, ensure=False)
+
+def extract_workload(sections, section_name):
+    """ Extract information related to SPEC and BASELINE """
+    wl = query(sections).where(lambda section: section['name'] == section_name) \
+                        .select(lambda section: section['metrics']) \
+                        .to_list()
+    return wl
+
+
+def extract_score(sections, section_name, node):
+    """ Extract information related to QPI """
+    qpi = query(sections).where(lambda child: child['name'] == node) \
+                         .select_many(lambda child: child['children']) \
+                         .where(lambda child: child['name'] == section_name) \
+                         .to_list()
+    return qpi
 
 
 @click.group()
-@pass_context
-def cli(ctx):
+def cli():
     """ View QTIP results"""
     pass
 
 
 @cli.command('show')
-@click.argument('metric')
-@click.option('-p', '--path', help='Path to result directory')
-@pass_context
-def show(ctx, metric, path):
+@click.option('-n', '--node', help="Compute node in OPNFV cluster")
+@click.argument('section-name')
+def show(node, section_name):
     reporter = ConsoleReporter({})
-    report = reporter.render(metric, path)
-    click.echo(report)
+    report = reporter.render()
+    table_workload = PrettyTable(['workload', 'Description', 'Baseline',
+                                  'Results', 'Score'])
+    table_workload.align = 'l'
+
+    spec = extract_workload(report['spec']['sections'], section_name)
+    baseline = extract_workload(report['baseline']['sections'], section_name)
+    scores = extract_score(report['qpi']['children'], section_name, node)
+
+    for sp, bl, score in zip(spec[0][0]['workloads'],
+                             baseline[0][0]['workloads'],
+                             scores[0]['children'][0]['children']):
+        table_workload.add_row([sp['name'],
+                               sp['description'],
+                               bl['baseline'], 1, score['score']])
+    click.echo(table_workload)

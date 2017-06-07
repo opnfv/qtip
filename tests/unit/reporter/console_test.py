0b0c704fe3b491588ce4ff4e4a04acb850333f1a
Taseer <taseer94@gmail.com>
2017-06-07 22:06:26 +0500

    (WIP) Integrate report with CLI
    - Merge scores and results in qpi.json
    
    JIRA:QTIP-244
    
    Change-Id: Ie349d8da94dd4fc3cc9c746edc92083f63429dc9
    Signed-off-by: Taseer Ahmed <taseer94@gmail.com>

--- tests/unit/reporter/console_test.py
+++ tests/unit/reporter/console_test.py
@@ -18,27 +18,5 @@ def console_reporter():
     return ConsoleReporter({})
 
 
-@pytest.fixture
-def result_path():
-    result = path.join(path.dirname(__file__), path.pardir, path.pardir,
-                       'data/reporter')
-    return result
-
-
 def test_constructor(console_reporter):
     assert isinstance(console_reporter, ConsoleReporter)
-
-
-@pytest.mark.parametrize(['template_name'], [
-    ('dhrystone',),
-    ('whetstone',),
-    ('dpi',),
-    ('ramspeed',),
-    ('ssl',)
-])
-def test_templates(template_name, console_reporter, result_path):
-    """ Test dhrystone report"""
-
-    result = console_reporter.render(template_name, result_path)
-    for line in result.split('\n'):
-        assert len(line) <= 80

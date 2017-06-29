6bff290c5709bf86e7ff9426ab839a602607c870
Taseer <taseer94@gmail.com>
2017-06-29 05:24:25 +0500

    Integrate Report with CLI.
    - Migrate fixtures to conftest to be used by multiple files
    
    Change-Id: I9a705b93189ac1f2e6fd2fc4e3f05aec7af379f1
    Signed-off-by: Taseer Ahmed <taseer94@gmail.com>

--- tests/unit/reporter/console_test.py
+++ tests/unit/reporter/console_test.py
@@ -8,7 +8,6 @@
 ##############################################################################
 
 import pytest
-from os import path
 
 from qtip.reporter.console import ConsoleReporter
 
@@ -18,27 +17,5 @@ def console_reporter():
     return ConsoleReporter({})
 
 
-@pytest.fixture
-def result_path():
-    result = path.join(path.dirname(__file__), path.pardir, path.pardir,
-                       'data/reporter')
-    return result
-
-
 def test_constructor(console_reporter):
     assert isinstance(console_reporter, ConsoleReporter)
-
-
-@pytest.mark.parametrize(['template_name'], [
-    ('dhrystone',),
-    ('whetstone',),
-    ('dpi',),
-    ('ramspeed',),
-    ('ssl',)
-])
-def test_templates(template_name, console_reporter, result_path):
-    """ Test dhrystone report"""
-
-    result = console_reporter.render(template_name, result_path)
-    for line in result.split('\n'):
-        assert len(line) <= 80

43c6ff7554222e4e3cf5075c29213a6f87c1dc01
akhilbatra898 <akhil.batra@research.iiit.ac.in>
2017-07-13 08:04:17 +0530

    Define Database models
     - Add dbfile to gitignore
     - Add models Repo and Task
     - Register models for Django Admin
    
    Change-Id: I46f23b0cb4deba2efc874cceb93251010ebb2860
    Signed-off-by: akhilbatra898 <akhil.batra@research.iiit.ac.in>

--- qtip/web/bench/admin.py
+++ qtip/web/bench/admin.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-# from django.contrib import admin
+from django.contrib import admin
+
+import models
 
 # Register your models here.
+
+
+admin.site.register(models.Repo)
+admin.site.register(models.Task)

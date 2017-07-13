2e4a3dfc8b45ae6dc72aa85510ce920dbafe54ad
akhilbatra898 <akhil.batra@research.iiit.ac.in>
2017-07-13 08:04:17 +0530

    Define Database models
     - Add dbfile to gitignore
     - Add models Repo and Task
     - Register models for Django Admin
    
    Change-Id: I46f23b0cb4deba2efc874cceb93251010ebb2860
    Signed-off-by: akhilbatra898 <akhil.batra@research.iiit.ac.in>

--- qtip/web/bench/admin.py
+++ qtip/web/bench/admin.py
@@ -1,6 +1,22 @@
+##############################################################################
+# Copyright (c) 2017 akhil.batra@research.iiit.ac.in and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
 # -*- coding: utf-8 -*-
+
 from __future__ import unicode_literals
 
-# from django.contrib import admin
+from django.contrib import admin
+
+import models
 
 # Register your models here.
+
+
+admin.site.register(models.Repo)
+admin.site.register(models.Task)
--- qtip/web/bench/models.py
+++ qtip/web/bench/models.py
@@ -1,6 +1,28 @@
+##############################################################################
+# Copyright (c) 2017 akhil.batra@research.iiit.ac.in and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-# from django.db import models
+from django.db import models
 
 # Create your models here.
+
+
+class Repo(models.Model):
+    name = models.CharField(max_length=200, blank=False)
+    github_link = models.URLField(unique=True)
+
+
+class Task(models.Model):
+    start_time = models.DateTimeField(auto_now_add=True)
+    end_time = models.DateTimeField()
+    run_time = models.DurationField()
+    repo = models.ForeignKey('Repo', on_delete=models.DO_NOTHING)
+    log = models.TextField()

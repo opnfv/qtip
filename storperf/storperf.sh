3b5aa20e07befdc3cf6b5db35a849a2ba2b86323
zhihui wu <wu.zhihui1@zte.com.cn>
2017-07-25 17:37:39 +0800

    Integrate with storperf
    Local test with apex is OK.
    usage: storperf.sh -t isntaller_type -i installer_ip -s stack_json_file -j job_json_file
    
    e.g. stack_json_file:
    {
        "agent_count": 2,
        "agent_image": "Ubuntu 16.04 x86_64",
    	"public_network": "external",
        "volume_size": 2
    }
    jos_json_file:
    {
    	"block_sizes": "1024",
    	"deadline": 30,
    	"steady_state_samples": 10,
    	"queue_depths": "1",
    	"workload": "wr,rr,rw",
    	"metadata": {
    		"disk_type": "HDD",
    		"scenario_name": "odl-l2",
    		"storage_node_count": 2,
    		"version": "danube",
    		"build_tag": "danube",
    		"test_case": "snia_steady_state"
    	}
    }
    
    Change-Id: I47c55ddd88f9722ce83b73a4d7ec91ece2aab2ed
    Signed-off-by: zhihui wu <wu.zhihui1@zte.com.cn>

---
+++ storperf/storperf.sh
@@ -0,0 +1,85 @@
+#! /bin/bash
+##############################################################################
+# Copyright (c) 2017 ZTE and others.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+usage(){
+   echo "usage: $0 -t <installer_type> -i <installer_ip> -s <stack_json_path> -j <job_json_path> " >&2
+}
+
+#Get options
+while getopts ":t:i:s:j:he" optchar; do
+   case "${optchar}" in
+       t) installer_type=${OPTARG} ;;
+       i) installer_ip=${OPTARG} ;;
+       s) stack_json=${OPTARG} ;;
+       j) job_json=${OPTARG} ;;
+       h) usage
+          exit 0
+          ;;
+       *) echo "Non-option argument: '-${OPTARG}'" >&2
+          usage
+          exit 2
+          ;;
+   esac
+done
+
+if [[ -z $WORKSPACE ]];then
+    WORKSPACE=`pwd`
+fi
+
+#set vars from env if not provided by user as options
+installer_type=${installer_type:-$INSTALLER_TYPE}
+installer_ip=${installer_ip:-$INSTALLER_IP}
+stack_json=${stack_json:-"$WORKSPACE/default_stack.json"}
+job_json=${job_json:-"$WORKSPACE/default_job.json"}
+
+source ./openstack.sh
+source ./storperf_docker.sh
+
+git clone --depth 1 https://gerrit.opnfv.org/gerrit/storperf $WORKSPACE/storperf
+git clone --depth 1 https://gerrit.opnfv.org/gerrit/releng $WORKSPACE/releng
+
+virtualenv $WORKSPACE/storperf_venv
+source $WORKSPACE/storperf_venv/bin/activate
+
+pip install -r ./storperf_requirements.txt
+
+$WORKSPACE/releng/utils/fetch_os_creds.sh -i ${installer_type} -a ${installer_ip} -d $WORKSPACE/openrc
+source $WORKSPACE/openrc
+
+grep "export" $WORKSPACE/openrc | sed "s/export //"  > $WORKSPACE/admin.rc
+echo "INSTALLER_TYPE=${installer_type}" >> $WORKSPACE/admin.rc
+export ENV_FILE=$WORKSPACE/admin.rc
+
+if [[ ! -d $WORKSPACE/carbon ]];then
+    mkdir -p $WORKSPACE/carbon
+    sudo chown 33:33 $WORKSPACE/carbon
+fi
+export CARBON_DIR=$WORKSPACE/carbon/
+
+delete_storperf_stack
+load_ubuntu_image
+create_storperf_flavor
+
+cd $WORKSPACE/storperf/docker
+echo "Clean existing storperf containers"
+clean_storperf_container
+echo "Launch new storperf containers"
+launch_storperf_container
+
+cd $WORKSPACE
+./start_job.sh -s $stack_json -j $job_json
+
+echo "Clean up environment"
+cd $WORKSPACE/storperf/docker
+clean_storperf_container
+openstack flavor delete storperf
+openstack image delete "Ubuntu 16.04 x86_64"
+
+echo "Done!"
+

bf868f99141ccfc6bc1bd3d7163745d542d230b3
Taseer Ahmed <taseer94@gmail.com>
2017-08-07 23:42:37 +0500

    Delete outdated code
    Change-Id: I0505d3ccc1ea76078f4d307f7d5ef66777ff6eb7
    Signed-off-by: Taseer Ahmed <taseer94@gmail.com>

--- qtip/cli/commands/cmd_qpi.py
+++ qtip/cli/commands/cmd_qpi.py
@@ -9,13 +9,6 @@
 
 
 import click
-from colorama import Fore
-import os
-
-from qtip.base.error import InvalidContentError
-from qtip.base.error import NotFoundError
-from qtip.cli import utils
-from qtip.loader.qpi import QPISpec
 
 
 @click.group()
@@ -26,30 +19,10 @@ def cli():
 
 @cli.command('list', help='List all the QPI specs')
 def cmd_list():
-    qpis = QPISpec.list_all()
-    table = utils.table('QPIs', qpis)
-    click.echo(table)
+    pass
 
 
 @cli.command('show', help='View details of a QPI')
 @click.argument('name')
 def show(name):
-    try:
-        qpi = QPISpec('{}.yaml'.format(name))
-    except NotFoundError as nf:
-        click.echo(Fore.RED + "ERROR: qpi spec: " + nf.message)
-    except InvalidContentError as ice:
-        click.echo(Fore.RED + "ERROR: qpi spec: " + ice.message)
-    else:
-        cnt = qpi.content
-        output = utils.render('qpi', cnt)
-        click.echo(output)
-
-
-@cli.command('run', help='Run performance tests for the specified QPI')
-@click.argument('name')
-@click.option('-p', '--path', help='Path to store results')
-def run(name, path):
-    runner_path = path.join(os.path.dirname(__file__), os.path.pardir, os.path.pardir,
-                            'runner/runner.py')
-    os.system('python {0} -b all -d {1}'.format(runner_path, path))
+    pass

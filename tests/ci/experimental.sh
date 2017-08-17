9a9f5ee0557505bc1710ab6bb9b80d40f94daba6
Yujun Zhang <zhang.yujunz@zte.com.cn>
2017-08-17 15:02:24 +0800

    DO NOT MERGE
    checking periodic task
    
    Change-Id: I44da35331949aaf7f0286bf9ac0db9c8aa7a747b
    Signed-off-by: Yujun Zhang <zhang.yujunz@zte.com.cn>

---
+++ tests/ci/experimental.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+export DOCKER_TAG=${DOCKER_TAG:-latest}
+export DEPLOY_SCENARIO=${DEPLOY_SCENARIO:-generic}
+export TEST_SUITE='compute'
+
+# See https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+$script_dir/periodic.sh

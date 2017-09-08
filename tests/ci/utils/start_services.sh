3063541d7ce0504d44ab148f2d086a17c67a7a90
Yujun Zhang <zhang.yujunz@zte.com.cn>
2017-09-08 17:08:59 +0800

    Refactor CI scripts
    - Move scripts executed inside container to qtip/scripts
    - tests/ci is limited to setup the environment and start services
    
    Change-Id: I454c715890601c6c6472006c2f0f6131695c2f51
    Signed-off-by: Yujun Zhang <zhang.yujunz@zte.com.cn>

---
+++ tests/ci/utils/start_services.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# detect installer_ip
+case ${INSTALLER_TYPE} in
+    apex)
+        export INSTALLER_IP=$(sudo virsh domifaddr undercloud | grep ipv4 | awk '{print $4}' | cut -d/ -f1)
+        ;;
+esac
+
+export ENV_FILE="${ENV_FILE:-$(pwd)/env_file}"
+cat << EOF > $ENV_FILE
+INSTALLER_TYPE=${INSTALLER_TYPE}
+INSTALLER_IP=${INSTALLER_IP}
+TEST_SUITE=${TEST_SUITE}
+NODE_NAME=${NODE_NAME:-opnfv-pod}
+SCENARIO=${DEPLOY_SCENARIO:-generic}
+TESTAPI_URL=${TESTAPI_URL:-}
+EOF
+
+TMPFILE=`mktemp /tmp/qtip.XXXXXX` || exit 1
+curl https://git.opnfv.org/releng/plain/utils/fetch_os_creds.sh | bash -s -- \
+    -i ${INSTALLER_TYPE} -a ${INSTALLER_IP} -d ${TMPFILE}
+grep "export" ${TMPFILE} | sed "s/export //" >> $ENV_FILE
+# eliminate warning message "Invalid -W option ignored: invalid action: '"ignore'"
+sed -i '/^PYTHONWARNINGS=/d' $ENV_FILE
+
+docker-compose -f $script_dir/${TEST_SUITE}/docker-compose.yaml up -d

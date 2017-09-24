4af949088fab66d473bddd9eb58c18a768afed98
Taseer Ahmed <taseer94@gmail.com>
2017-09-24 14:47:09 +0500

    (WIP) Add support for Mirantis Cloud Platform.
    JIRA: QTIP-261
    
    Change-Id: Ied9c3d91b25bc8dad7deb0cf30f83cd2b7855a92
    Signed-off-by: Taseer Ahmed <taseer94@gmail.com>

---
+++ qtip/ansible_library/modules/mcp.py
@@ -0,0 +1,121 @@
+#!/usr/bin/python
+
+###############################################################
+# Copyright (c) 2017 ZTE Corporation and others
+# taseer94@gmail.com
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+import json
+
+from ansible.module_utils.basic import AnsibleModule
+
+ANSIBLE_METADATA = {'metadata_version': '1.0',
+                    'status': ['preview'],
+                    'supported_by': 'community'}
+
+DOCUMENTATION = '''
+---
+module: mcp
+short_description: collecting facts from mcp environments
+description:
+    - Use this module to create a dynamic inventory from salt master (mcp).
+version_added: "1.0"
+author: "Taseer Ahmed (@Taseer)"
+options:
+notes:
+requirements:
+    - Host 'salt-master' is in ~/.ssh/config
+'''
+
+RETURN = '''
+ansible_facts:
+  description: facts collected for ansible
+  returned: success
+  type: dictionary
+  contains:
+    hosts:
+      description: host grouped by hostname, cluster, role and manufacture
+      type: dict
+    hosts_meta:
+      description: hosts meta data indexed by hostname
+      type: dict
+'''
+
+EXAMPLES = '''
+---
+- hosts: salt-master
+  tasks:
+  - name: collect facts of mcp hosts
+    mcp:
+  - debug: var=hostvars
+  - name: add compute node to ansible inventory
+    add_host:
+      name: "{{ hosts_meta[item]['ip'] }}"
+      groups: mcp-compute
+      ansible_user: root
+      ansible_ssh_common_args: '-o StrictHostKeyChecking=No -o ProxyJump=salt-master'
+    with_items: "{{ hosts.compute }}"
+- hosts: mcp-compute
+  tasks:
+  - name: check ssh connection
+    ping:
+'''
+
+
+def generate_inventory(nodes):
+    """Generate ansible inventory from node list in json format"""
+
+    hosts = defaultdict(list)
+    hosts_meta = {}
+
+    for node in nodes:
+        # skip deleting, offline, deploying and discovering/unprovisioned nodes
+        if node['pending_deletion'] or (not node['online']) \
+                or node['status'] == 'deploying' or node['status'] == 'discover':
+            continue
+
+        hostname = node['hostname']
+        cluster_id = node['cluster']
+        hw_vendor = node['meta']['system']['manufacturer'].lower()
+
+        [hosts[role.strip()].append(hostname) for role in node['roles'].split(",")]
+        hosts["cluster-{0}".format(cluster_id)].append(hostname)
+        hosts["hw-{0}-servers".format(hw_vendor)].append(hostname)
+
+        node_meta = {
+            'name': node['name'],
+            'online': node['online'],
+            'os_platform': node['os_platform'],
+            'status': node['status'],
+            'ip': node['ip'],
+            'mac': node['mac'],
+            'cluster': cluster_id,
+            'ansible_ssh_host': node['ip'],
+            'ansible_user': 'root'
+        }
+        hosts_meta[hostname] = node_meta
+
+    return {'hosts': hosts, 'hosts_meta': hosts_meta}
+
+
+def main():
+    module = AnsibleModule(argument_spec=dict())
+
+    cmd = [module.get_bin_path('salt', True), '--out=json', 'cmp*', 'network.get_hostname']
+    (rc, out, err) = module.run_command(cmd)
+
+    if rc is not None and rc != 0:
+        return module.fail_json(msg=err)
+
+    nodes = json.loads(out)
+
+    module.exit_json(changed=False, ansible_facts=generate_inventory(nodes))
+
+
+if __name__ == '__main__':
+    main()

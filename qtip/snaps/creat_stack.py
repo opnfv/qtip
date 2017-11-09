a2b4689a21368f424bcb29b4c54e69829d36b585
Taseer Ahmed <taseer94@gmail.com>
2017-11-09 10:59:29 +0500

    Add SNAPS basic skeleton
    JIRA: QTIP-284
    
    Change-Id: I121b483e165f065afb26f33b330145c75cd8cf89
    Signed-off-by: Taseer Ahmed <taseer94@gmail.com>

---
+++ qtip/snaps/creat_stack.py
@@ -0,0 +1,13 @@
+##############################################################################
+# Copyright (c) 2017 taseer94@gmail.com and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+from snaps.snaps.openstack import create_stack
+
+
+stack = create_stack.OpenStackHeatStack()
